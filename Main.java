360dd05b8f3a0a739266dc08305050b2274f45c1
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2024-11-25 13:39:14 -0500

    changed in the useraction and main file

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class Main{
     public static void main(String[] args){
-
+        User user = new User("Shorena Anzhilov", "Shorenaa@gmail.com");
+        System.out.println("User Created: " + user);
     }
 }
