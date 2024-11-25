6742d7bed5b8a09836ac9313f2395aa69d643a8f
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2024-11-25 13:44:13 -0500

    Noficatinmanger created

--- NotificationInterface.java
+++ NotificationInterface.java
@@ -1,9 +1,9 @@
 // notification intreface
 
- public interface NotificationInterface {
-
-   void send();
-  String getMessage();
-//     User getTaragetUser();
-
+public interface NotificationInterface {
+  void send();        
+  // Get the notification message               
+  String getMessage();     
+  // Get the user who will receive the notification           
+  User getTargetUser();               
 }
--- NotificationManeger.java
+++ NotificationManeger.java
@@ -1,6 +1,15 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationManeger implements NotificationInterface{
+
+    //to list the notifications
+  private List<NotificationInterface> notofocations;
+
+  public NotificationManeger(){
+    this.notofocations = new ArrayList<>();
+  }
+
   
 
 }
