f184530b87fd3137d2b3ac3e252eb1d93b29f180
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2024-11-26 09:07:37 -0500

    reply.java started

--- Reaction.java
+++ Reaction.java
@@ -5,4 +5,14 @@ public class Reaction extends AbstractUserContent {
         super(sender, reactionType.toString()); // Use reactionType as content
         this.reactionType = reactionType;
     }
+
+    // Get the type of reaction
+    public ReactionType getReactionType() {
+        return reactionType;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Reaction by " + sender.getName() + ": " + reactionType);
+    }
 }
--- Reply.java
+++ Reply.java
@@ -1,4 +1,16 @@
-public class Reply {
+import java.util.ArrayList;
+import java.util.List;
 
-    
+public class Reply extends AbstractUserContent {
+    private List<Reply> replies; // Nested replies
+
+    public Reply(User sender, String content) {
+        super(sender, content);
+        this.replies = new ArrayList<>();
+    }
+
+    // Get all nested replies
+    public List<Reply> getReplies() {
+        return replies;
+    }
 }
