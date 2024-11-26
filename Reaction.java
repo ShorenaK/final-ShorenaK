46841cd8f3e49d1b0e5e0fdcf20b1d2d2fe5866f
Shorena K. Anzhilov <shorenaanzhilov@gmail.com>
2024-11-26 09:02:49 -0500

    Reaction.java filr

--- Reaction.java
+++ Reaction.java
@@ -1,4 +1,8 @@
-public class Reaction {
+public class Reaction extends AbstractUserContent {
+    private ReactionType reactionType; // Type of reaction (LIKE, LOVE, etc.)
 
-    
+    public Reaction(User sender, ReactionType reactionType) {
+        super(sender, reactionType.toString()); // Use reactionType as content
+        this.reactionType = reactionType;
+    }
 }
